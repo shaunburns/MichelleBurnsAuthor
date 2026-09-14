f1074e2d6497125e83770a6f3729a791b42ce56c
Shaun Burns <25629071+shaunburns@users.noreply.github.com>
2026-09-14 16:23:26 +1000

    Add CI deployment to Pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,11 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://MichelleBurnsAuthor.com',
+  url: 'https://shaunburns.github.io',
+  //url: 'https://MichelleBurnsAuthor.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/MichelleBurnsAuthor',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
