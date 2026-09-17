65413bd49eae24f3a43472392d5e919415291049
Shaun Burns <25629071+shaunburns@users.noreply.github.com>
2026-09-17 16:22:03 +1000

    Minor improvements to text for Psycharium book and About Me

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -17,13 +17,13 @@ export default function About() {
           width: '50vw',
         }}>
         <p>
-          Michelle is a lifelong enthusiast of high fantasy literature whose passion for immersive storytelling has inspired her to contribute her own voice to the genre she has loved for decades. In 2025, she made the bold decision to pursue a new career path as an author, embarking on the creation of The Psycharium, the first novel in a planned three-book fantasy series.
+          Michelle is a lifelong enthusiast of fantasy literature whose passion for immersive storytelling has inspired her to contribute her own voice to the genre she has loved for decades. In 2025, she made the bold decision to pursue a new career path as an author, embarking on the creation of The Psycharium, the first novel in a planned three-book fantasy series.
         </p>
         <p>
           An avid reader and dedicated storyteller, Michelle believes in the transformative power of books to spark imagination, curiosity, and connection. Her love of literature extends beyond her own writing, as she shares that passion with her children through a cherished nightly tradition of reading together. By fostering a love of books in the next generation, she hopes to inspire the same sense of wonder and adventure that has shaped her own life.
         </p>
         <p>
-          Through her writing, Michelle aims to craft compelling worlds, memorable characters, and meaningful stories that resonate with fantasy readers while honoring the genre that first ignited her imagination.
+          Through her writing, Michelle aims to craft compelling worlds, memorable characters, and meaningful stories that resonate with readers while honoring the genre that first ignited her imagination.
         </p>
       </div>
     </Layout>
