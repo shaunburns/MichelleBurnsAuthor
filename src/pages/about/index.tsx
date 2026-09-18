6725d2b9ddcc7b9b15707751beae099f3f63e201
Shaun Burns <25629071+shaunburns@users.noreply.github.com>
2026-09-18 16:53:49 +1000

    Fixed CSS style issues for mobile browsers

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -11,18 +11,19 @@ export default function About() {
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
-          height: '50vh',
+          flexDirection: 'column',
+          minHeight: '50vh',
+          width: '90%',
+          maxWidth: '800px',
           fontSize: '20px',
-          flexDirection: 'column', 
-          width: '50vw',
         }}>
-        <p>
+        <p className={styles.paragraphDiv}>
           Michelle is a lifelong enthusiast of fantasy literature whose passion for immersive storytelling has inspired her to contribute her own voice to the genre she has loved for decades. In 2025, she made the bold decision to pursue a new career path as an author, embarking on the creation of The Psycharium, the first novel in a planned three-book fantasy series.
         </p>
-        <p>
+        <p className={styles.paragraphDiv}>
           An avid reader and dedicated storyteller, Michelle believes in the transformative power of books to spark imagination, curiosity, and connection. Her love of literature extends beyond her own writing, as she shares that passion with her children through a cherished nightly tradition of reading together. By fostering a love of books in the next generation, she hopes to inspire the same sense of wonder and adventure that has shaped her own life.
         </p>
-        <p>
+        <p className={styles.paragraphDiv}>
           Through her writing, Michelle aims to craft compelling worlds, memorable characters, and meaningful stories that resonate with readers while honoring the genre that first ignited her imagination.
         </p>
       </div>
