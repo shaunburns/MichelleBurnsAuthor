e4c9ad6ec0dd48fbac7b645bc0498bfcbe713f34
Shaun Burns <25629071+shaunburns@users.noreply.github.com>
2026-09-18 15:42:26 +1000

    Updated URL to MichelleBurnsAuthor.com

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,11 +15,11 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://shaunburns.github.io',
-  //url: 'https://MichelleBurnsAuthor.com',
+  //url: 'https://shaunburns.github.io',
+  url: 'https://MichelleBurnsAuthor.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/MichelleBurnsAuthor',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
